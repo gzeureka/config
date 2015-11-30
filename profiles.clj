6292b24c9e30d4857051941e96e5902e302423dd
gzeureka <gzeureka@gmail.com>
2015-11-30 17:42:19 +0800

    Add slamhound

--- profiles.clj
+++ profiles.clj
@@ -1,2 +1,5 @@
 {:user {:plugins [[lein-localrepo "0.5.3"]
-                  [lein-cprint "1.2.0"]]}}
+                  [lein-cprint "1.2.0"]]
+        :dependencies [[slamhound "1.5.5"]]
+        :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
+
