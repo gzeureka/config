cfd69c8a43d80cd459fda71d9658fb07b5923cfc
gzeureka <gzeureka@gmail.com>
2016-03-17 16:19:54 +0800

    comment out pretty

--- profiles.clj
+++ profiles.clj
@@ -1,5 +1,8 @@
 {:user {:plugins [[lein-localrepo "0.5.3"]
-                  [lein-cprint "1.2.0"]]
-        :dependencies [[slamhound "1.5.5"]]
+                  [lein-cprint "1.2.0"]
+                  ;[io.aviso/pretty "0.1.24"]
+                  ]
+        :dependencies [[slamhound "1.5.5"]
+                       [table "0.5.0"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
 
