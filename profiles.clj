1722528491ee436d1d6e88fbeef2798f740a18ed
gzeureka <gzeureka@gmail.com>
2017-04-16 11:38:58 +0800

    add lein-release

--- profiles.clj
+++ profiles.clj
@@ -1,5 +1,7 @@
 {:user {:plugins [[lein-localrepo "0.5.3"]
+                  [lein-release "1.0.5"]
                   [lein-cprint "1.2.0"]
+                  ;[cider/cider-nrepl "0.14.0-snapshot"]
                   ;[io.aviso/pretty "0.1.24"]
                   ]
         :dependencies [[slamhound "1.5.5"]
